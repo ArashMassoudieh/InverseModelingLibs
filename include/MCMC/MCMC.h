23029e6704161d04f92b7c02e0717b9078b63eaf
Arash Massoudieh <arashmassoudieh@gmail.com>
2025-10-26 23:10:44 -0400

    Updated MCMC.h

--- include/MCMC/MCMC.h
+++ include/MCMC/MCMC.h
@@ -547,7 +547,7 @@ private:
 
     // Legacy compatibility data
     TimeSeriesSet<double> measuredData;      ///< Measured data storage
-    bool jacobian_Multiplier = 0;
+    bool jacobian_Multiplier = 1;
 };
 
 #include "MCMC.hpp"
